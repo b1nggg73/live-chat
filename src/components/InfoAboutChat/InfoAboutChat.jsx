47bcc20b4a1c179a36f98f4618aab3d03fd13a92
Maksim <przedviralem@gmail.com>
2024-07-05 22:27:16 +0300

    top made, layout 20%

--- src/components/InfoAboutChat/InfoAboutChat.jsx
+++ src/components/InfoAboutChat/InfoAboutChat.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import btnFavorite from '../../img/infoAboutChat/btnFavorite.svg';
+import usersCounts from '../../img/infoAboutChat/usersCounts.svg';
+import notification from '../../img/infoAboutChat/notification.svg';
+import setting from '../../img/infoAboutChat/setting.svg';
 import './InfoAboutChat.scss';
 
 const InfoAboutChat = () => {
   return (
     <div className="info-about-chat">
-      <div className="name-chat"></div>
+      <div className="info-about-chat-left">
+        <div className="name-chat">#general</div>
+        <div className="btn-favorite">
+          <img src={btnFavorite} alt="" />
+        </div>
+      </div>
+      <div className="info-about-chat-right">
+        <div className="usersCounts">
+          <img src={usersCounts} alt="" />
+          <div className="numbersCounts">1,093</div>
+        </div>
+        <input className="searchMessages" type="text" placeholder="Search..." />
+        <div className="notification">
+          <img src={notification} alt="" />
+        </div>
+        <div className="setting">
+          <img src={setting} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
